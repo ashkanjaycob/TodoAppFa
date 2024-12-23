44e2443e51da7cc214ca1be1a56e619c4a7902fa
Ashkan Yaghobi <ashkan.yaghobii@gmail.com>
2024-12-23 12:02:44 +0330

    Added logic For Todo Create with Validation

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const TodoInput = document.getElementById("inputTodo");
 const AddBtn = document.getElementById("addTodo");
+const todoList = document.getElementById("showTodos");
 
 AddBtn.addEventListener("click", () => {
-  alert(TodoInput.value.trim());
-  console.log("test input");
+  const Text = TodoInput.value.trim();
+  if (Text) {
+    const li = document.createElement("li");
+    li.innerHTML = `
+            <span>${Text}</span>
+    `;
+    todoList.appendChild(li);
+    TodoInput.value = "";
+  } else {
+    alert(".لطفا مقدار متن را وارد کنید ");
+  }
 });
