7508120aa4f5796fc1f4baeed666b15ae89e8ed9
Ashkan Yaghobi <ashkan.yaghobii@gmail.com>
2024-12-23 14:46:09 +0330

    Fix updateCloseButtons( )

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const TodoInput = document.getElementById("inputTodo");
 const AddBtn = document.getElementById("addTodo");
 let todoList = document.getElementById("showTodos");
 const clearTodos = document.getElementById("clearTodos");
-const searchBox = document.getElementById("searchTodo"); 
+const searchBox = document.getElementById("searchTodo");
 
 AddBtn.addEventListener("click", () => {
   const Text = TodoInput.value.trim();
@@ -29,13 +29,13 @@ AddBtn.addEventListener("click", () => {
   }
 });
 
-// delete item from todos
+// Delete item from todos
 function updateCloseButtons() {
   const close = document.getElementsByClassName("close");
   for (let i = 0; i < close.length; i++) {
     close[i].onclick = function () {
-      var div = this.parentElement;
-      div.style.display = "none";
+      const li = this.parentElement;
+      li.remove(); // Remove element for Save in Local Storage
       saveTodos();
     };
   }
@@ -56,59 +56,58 @@ clearTodos.addEventListener("click", () => {
   saveTodos();
 });
 
-
 searchBox.addEventListener("input", () => {
-    const filter = searchBox.value.toLowerCase();
-    const items = todoList.getElementsByTagName("li");
-  
-    // Loop on Todos
-    for (let i = 0; i < items.length; i++) {
-      const itemText = items[i].textContent || items[i].innerText;
-      if (itemText.toLowerCase().indexOf(filter) > -1) {
-        items[i].style.display = "";
-      } else {
-        items[i].style.display = "none";
-      }
-    }
-  });
+  const filter = searchBox.value.toLowerCase();
+  const items = todoList.getElementsByTagName("li");
 
-function saveTodos() {
-    const todos = [];
-    const items = todoList.getElementsByTagName("li");
-  
-    for (let i = 0; i < items.length; i++) {
-      const todoText = items[i].querySelector("span").textContent;
-      const isChecked = items[i].classList.contains("line-through");
-      todos.push({ text: todoText, checked: isChecked });
-    }
-  
-    localStorage.setItem("todos", JSON.stringify(todos));
-}
-
-// Load todos Based on Add todo 
-function loadTodos() {
-    const storedTodos = JSON.parse(localStorage.getItem("todos"));
-    if (storedTodos && Array.isArray(storedTodos)) {
-      storedTodos.forEach((todo) => {
-        const li = document.createElement("li");
-        li.innerHTML = `
-                <span>${todo.text}</span>
-          `;
-        const span = document.createElement("SPAN");
-        const txt = document.createTextNode("\u00D7");
-        span.className = "close";
-        span.appendChild(txt);
-        li.appendChild(span);
-  
-        if (todo.checked) {
-          li.classList.add("line-through");
-        }
-        todoList.appendChild(li);
-      });
-  
-      updateCloseButtons();
+  // Loop on Todos
+  for (let i = 0; i < items.length; i++) {
+    const itemText = items[i].textContent || items[i].innerText;
+    if (itemText.toLowerCase().indexOf(filter) > -1) {
+      items[i].style.display = "";
+    } else {
+      items[i].style.display = "none";
     }
   }
-  
+});
+
+function saveTodos() {
+  const todos = [];
+  const items = todoList.getElementsByTagName("li");
+
+  for (let i = 0; i < items.length; i++) {
+    const todoText = items[i].querySelector("span").textContent;
+    const isChecked = items[i].classList.contains("line-through");
+    todos.push({ text: todoText, checked: isChecked });
+  }
+
+  localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+// Load todos Based on Add todo
+function loadTodos() {
+  const storedTodos = JSON.parse(localStorage.getItem("todos"));
+  if (storedTodos && Array.isArray(storedTodos)) {
+    storedTodos.forEach((todo) => {
+      const li = document.createElement("li");
+      li.innerHTML = `
+                <span>${todo.text}</span>
+          `;
+      const span = document.createElement("SPAN");
+      const txt = document.createTextNode("\u00D7");
+      span.className = "close";
+      span.appendChild(txt);
+      li.appendChild(span);
+
+      if (todo.checked) {
+        li.classList.add("line-through");
+      }
+      todoList.appendChild(li);
+    });
+
+    updateCloseButtons();
+  }
+}
+
 // For First Render
 loadTodos();
